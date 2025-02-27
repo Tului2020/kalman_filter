5a74c2c85d66e410401352f3fb98a381063c7ad9
Tului Gantulga <tului_2020@yahoo.com>
2025-02-27 15:35:01 -0800

    implemented EKF (nonlinear) case for circular motion with only positisions as x_initial

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
-use nalgebra::{ArrayStorage, Const, Matrix};
+use nalgebra::{SVector, Vector2};
 
-pub fn circular_motion(time: f64) -> Matrix<f64, Const<2>, Const<1>, ArrayStorage<f64, 2, 1>> {
-    Matrix::<f64, Const<2>, Const<1>, ArrayStorage<f64, 2, 1>>::new(time.cos(), time.sin())
+pub fn circular_motion(time: f64) -> SVector<f64, 2> {
+    Vector2::new(time.cos(), time.sin())
 }
 
-pub fn circular_motion_vel(time: f64) -> Matrix<f64, Const<2>, Const<1>, ArrayStorage<f64, 2, 1>> {
-    Matrix::<f64, Const<2>, Const<1>, ArrayStorage<f64, 2, 1>>::new(-time.sin(), time.cos())
+pub fn circular_motion_vel(time: f64) -> SVector<f64, 2> {
+    Vector2::new(-time.sin(), time.cos())
+}
+
+pub fn add_noise(state: SVector<f64, 2>, noise: f64) -> SVector<f64, 2> {
+    state + Vector2::new(rand::random::<f64>() * noise, rand::random::<f64>() * noise)
 }
