4d19fcaac12f28cfd6d1b4579c334f93fb2121f7
Tului Gantulga <tului_2020@yahoo.com>
2025-03-18 09:27:36 -0700

    removed russel stuff because it's missing dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod control;
 pub mod data;
 pub mod localization;
-pub mod mapping;
+// pub mod mapping;
 pub mod models;
 pub mod utils;
--- src/mapping/mod.rs
+++
@@ -1,6 +0,0 @@
-mod ekf_slam_known;
-mod g2o;
-mod pose_graph_optimization;
-mod se2_se3;
-
-pub use pose_graph_optimization::{PoseGraph, PoseGraphSolver};
